e93802345060729836e753e3511f4da042322503
dayan.li@datayes.com <dayan.li@datayes.com>
2016-09-11 22:34:23 +0800

    test for rest_framework --- permissions

--- learning/views.py
+++ learning/views.py
@@ -1,24 +1,27 @@
 
-from rest_framework import generics, permissions, viewsets
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.permissions import DjangoModelPermissions
+from rest_framework.viewsets import ModelViewSet
 from django.contrib.auth.models import User
 from serializers import UserSerializer, SnippetSerializer
 from models import Snippet
+from permissions import IsOwnerOrReadOnly
 
 # Create your views here.
-class SnippetViewSet(viewsets.ModelViewSet):
+class SnippetViewSet(ModelViewSet):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (DjangoModelPermissions, IsOwnerOrReadOnly, )
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
 
-class UserList(generics.ListAPIView):
+class UserList(ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
-class UserDetail(generics.RetrieveAPIView):
+class UserDetail(RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
